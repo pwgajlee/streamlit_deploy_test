98cdc19cd8fefe98b1db52aa62ef95d0944e8d27
pwgajlee <119456563+pwgajlee@users.noreply.github.com>
2022-11-29 13:57:21 -0800

    Add files via upload

---
+++ AllContributions_test.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+
+#reading data file (csv)
+data = pd.read_csv(r'C:\Users\ghduf\venv\contributions_w.csv')
+
+#formatting $ from string to float
+data['ConsolidatedContribution']=data['ConsolidatedContribution'].str.replace(',','')
+data.ConsolidatedContribution = data.ConsolidatedContribution.astype('float32')
+data['HealthContribution']=data['HealthContribution'].str.replace(',','')
+data.HealthContribution = data.HealthContribution.astype('float32')
+data['PensionContribution']=data['PensionContribution'].str.replace(',','')
+data.PensionContribution = data.PensionContribution.astype('float32')
+
+#rename column names
+data.columns = ['Quarter', 'Consolidated Contributions', 'Health Contributions', 'Pension Contributions']
+
+#combine quarters and sum contributions
+agg_functions = {'Consolidated Contributions': 'sum', 'Health Contributions': 'sum', 'Pension Contributions': 'sum'}
+data_clean = data.groupby(data['Quarter']).aggregate(agg_functions)
+
+#create new dataframe just for table presentation
+data_table = data_clean.copy()
+
+#formatting $ to millions, rounding to one decimal place, and cleaning table
+data_table['Consolidated'] = '$' + (data_table['Consolidated Contributions']/1000000).round(decimals=1).astype(str) + 'M'
+data_table['Health Fund'] = '$' + (data_table['Health Contributions']/1000000).round(decimals=1).astype(str) + 'M'
+data_table['Pension Plan'] = '$' + (data_table['Pension Contributions']/1000000).round(decimals=1).astype(str) + 'M'
+data_table = data_table.drop(columns=['Consolidated Contributions', 'Health Contributions', 'Pension Contributions'])
+data_table = data_table.transpose()
+
+st.line_chart(data_clean)
+st.table(data_table)
+
